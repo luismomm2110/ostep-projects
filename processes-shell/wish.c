342957545429dfb9038116aba2d291573779b999
luismomm2110 <luismomm@DESKTOP-2A9EV4J.localdomain>
2021-05-06 14:23:15 -0300

    project 'processes-shell' passes 'ls' command with arguments

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -5,41 +5,52 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {		
-		
+	char error_message[30] = "An error has ocurred\n";
+
 	while (1) {	
 		printf("wish> ");
 		fflush(stdout);
 
+		int i = 0;
 		char *line = NULL;
 		size_t len = 0;		
 		char *myargv[20];
 		char *path[100]; 
-
-		path[0] = strdup("/bin/");
+		char *sliceLine;
+		
 		getline(&line, &len, stdin);
 		line[strlen(line) - 1] = 0;
+
+		while ( (sliceLine = strsep(&line, " ")) != NULL) {
+			myargv[i] = strdup(sliceLine);
+			i++;
+		}
+
+		myargv[i]= NULL;
+
+		if (strcmp(myargv[0], "exit") == 0) {
+			exit(0);
+		}
 
 		int rc = fork();
 
 		if (rc == 0) {
-			strcat(path[0], line);
+			
+			path[0] = strdup("/bin/");
+			strcat(path[0], myargv[0]);
 
 	      		if ((access(path[0], X_OK)) == -1) {
-				printf("Didn't find command\n");
+				write(STDERR_FILENO, error_message,
+					strlen(error_message));
 			}				
-			
-			myargv[0] = strdup("/bin/ls");
-			if (strcmp(myargv[0], path[0]) != 0) {
-				printf("not equal\n");
-			}			
-			
+				
 			myargv[0] = strdup(path[0]);
-			myargv[1] = NULL;			
 		
-			execv(myargv[0], myargv);	
-	
+			execv(myargv[0], myargv); 
+
 		} else {
 			rc = (int) wait(NULL);	
+			free(line);
 		}
 	}
 }
