3bacfa0b00a6de59abeab2dd88efcd11d4b5010d
luismomm2110 <luismomm@DESKTOP-2A9EV4J.localdomain>
2021-05-13 14:47:00 -0300

    todo: redirect to stderr of parent

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -3,6 +3,8 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
 
 /* error function */
 void errorMsg() {
@@ -17,17 +19,16 @@ int exitFunction(int numArgs) {
 	if (numArgs == 1) {
 		exit(0);	
 	}
-
 	return 0;
 }
-void cdFunction(int numArgs, char *path) { 
-	if (numArgs != 2) {
-		if (chdir(path) != -1) {
-			chdir(path);
-		}
-	}
-}
 
+void cdFunction(int numArgs, char *path) { 
+//	char s[100];
+/*	printf("%s\n", getcwd(s, 100));
+	chdir("..");
+	printf("%s\n", getcwd(s, 100)); */
+}
+//TODO
 /* process input slicing the lines and inserting in myargv */
 int processInput(char *userLine, char *userArgs[]) {
 	char *sliceLine;
@@ -46,7 +47,6 @@ int processInput(char *userLine, char *userArgs[]) {
 	return i;
 }
 
-
 int main(int argc, char *argv[]) {		
 	char *line = NULL;
 	size_t len = 0;		
@@ -54,6 +54,7 @@ int main(int argc, char *argv[]) {
 	char *path[100]; 
 	FILE *fp = stdin;
 	ssize_t nread;
+	int statval;
 
 	if (argc == 2) {
 		if ( (fp = fopen(argv[1], "r")) == NULL) {
@@ -63,6 +64,7 @@ int main(int argc, char *argv[]) {
 
 	while (1) {	
 		if (argc == 1) {
+
 			printf("wish> ");
 			fflush(stdout);
 		}	
@@ -80,11 +82,15 @@ int main(int argc, char *argv[]) {
 		if (strcmp(myargv[0], "cd") == 0) {
 			cdFunction(lenArray, myargv[1]);
 		}
-
+		
+		char *debugMessage = "write message for debug in STDERR\n";
+		
+		write(STDERR_FILENO, debugMessage, strlen(debugMessage));
+			
 		int rc = fork();
+		
 
 		if (rc == 0) {
-			
 			path[0] = strdup("/bin/");
 			strcat(path[0], myargv[0]);
 
@@ -94,16 +100,25 @@ int main(int argc, char *argv[]) {
 			}				
 				
 			myargv[0] = strdup(path[0]);
-		
-			execv(myargv[0], myargv); 
+			
+			execv(myargv[0], myargv);
 
 		} else {
-			rc = (int) wait(NULL);	
-		        fclose(fp);	
+			wait(&statval);
+ 			if (WIFEXITED(statval)) {
+				if (WEXITSTATUS(statval) != 0) {	
+					char *debugError = strerror(WEXITSTATUS(statval));
+					write(STDERR_FILENO, debugError,
+					strlen(debugError));
+				}
+			} else {
+				printf("not terminate with exit\n");
+			}
 			free(line);
+			line = NULL;
 		}
 	}
-
+	free(line);
+	fclose(fp);
 	return 0;
-
 }
