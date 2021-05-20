cce92a3a012f7ff7ab6bfea41dfe69e100f9fa54
luismomm2110 <luismomm@DESKTOP-2A9EV4J.localdomain>
2021-05-20 16:46:23 -0300

    test 7

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+
 /* error function */
 void errorMsg() {
 	char error_message[30] = "An error has occurred\n";
@@ -69,19 +70,37 @@ void redirectChild(FILE *in) {
 			errorMsg();
 			return;
 		}
+
 		fclose(in);
 	}
 }
+  
+int searchPath (char *paths[], char execvPath[], char *command) {
+	int i = 0;
+	
+	while (paths[i] != NULL) {
+		snprintf(execvPath, 512, "%s/%s", paths[i], command);
+		if (access(execvPath, X_OK) == 0) {
+			return 0;
+		}
+		i++;
+	}	
+	return -1;
+}
+
 
 int main(int argc, char *argv[]) {		
 	char *line = NULL;
 	size_t len = 0;		
 	char *myargv[20];
-	char *path[100]; 
+	char *paths[500]; 
 	FILE *fp = stdin;
 	ssize_t nread;
 	int statval;
 	FILE *out = stdout;
+
+	paths[0] = strdup("/bin");
+	paths[1] = NULL;
 
 	if (argc == 2) {
 		if ( (fp = fopen(argv[1], "r")) == NULL) {
@@ -111,23 +130,32 @@ int main(int argc, char *argv[]) {
 			continue;
 		}
 		
+		if (strcmp(myargv[0], "path") == 0) {
+			paths[0] = NULL;
+			int i = 0;
+			for (; i < lenArray -1; i++) {
+				paths[i] = strdup(myargv[i+1]);
+			}
+			
+			paths[i+1] = NULL;
+			continue;
+		}
+
 		int rc = fork();
 
 		if (rc == -1) {
 			errorMsg();
 		} else if (rc == 0) {
-			path[0] = strdup("/bin/");
-			strcat(path[0], myargv[0]);
-
-	      		if ((access(path[0], X_OK)) == -1) {
+			char execvPath[512];
+			int n = searchPath(paths, execvPath, myargv[0]);
+			if (n == -1) { 
 				errorMsg();
 				exit(0);
-			}				
+			}
 				
-			myargv[0] = strdup(path[0]);
 			redirectChild(out);
 			
-			execv(myargv[0], myargv);
+			execvp(execvPath, myargv);
 			errorMsg();
 			exit(0);
 
